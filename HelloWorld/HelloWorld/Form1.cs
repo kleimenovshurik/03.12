03e10ae76aefa26f7709699265d0a1e4aa3f6f87
alex <klfkn@mail.ru>
2017-01-14 19:09:00 +0400

    Что делает белка

--- HelloWorld/HelloWorld/Form1.cs
+++ HelloWorld/HelloWorld/Form1.cs
@@ -23,7 +23,7 @@ namespace HelloWorld
 
         private void button2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "Belka";
+            textBox1.Text = "Belka - ест орешки, а скорлупки золотые";
         }
     }
 }
